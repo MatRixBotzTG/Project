5855e8054e5b7e7380f2b4a537f8e06178d279b9
MatRixBotzTG <122761844+MatRixBotzTG@users.noreply.github.com>
2025-05-21 19:10:37 +0530

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "jobportal";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
